4a683904b31bed64fb727495ccc2980b08c76cfd
phelinor <gera.glez@gmail.com>
2019-03-27 15:46:50 -0600

    Ready Screen

--- GameShow/GameShow/ReadyScreen.Designer.cs
+++ GameShow/GameShow/ReadyScreen.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameShow
 {
-    partial class Form1
+    partial class ReadyScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ReadyScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ReadyScreen";
+            this.Text = "Ready Screen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- GameShow/GameShow/ReadyScreen.cs
+++ GameShow/GameShow/ReadyScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GameShow
 {
-    public partial class Form1 : Form
+    public partial class ReadyScreen : Form
     {
-        public Form1()
+        public ReadyScreen()
         {
             InitializeComponent();
         }
